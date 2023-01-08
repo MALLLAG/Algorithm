cf0380c3a4bf395c8bc21a1e3643e799acabd050
김성현 <beyool95@naver.com>
2023-01-08 14:28:21 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,12 +1,2 @@
-import math
-
 def solution(money):
-    result = []
-    if money < 5500:
-        result.append(0)
-        result.append(money)
-    else:
-        result.append(math.trunc(money / 5500))
-        result.append(money - (math.trunc(money / 5500) * 5500))
-
-    return result
+    return divmod(money, 5500)
