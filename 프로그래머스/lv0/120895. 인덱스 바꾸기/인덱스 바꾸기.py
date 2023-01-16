46a8865ce3c17e33e929c8b6822184c100d2d8db
김성현 <beyool95@naver.com>
2023-01-16 20:13:42 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,11 +1,4 @@
 def solution(my_string, num1, num2):
     l = list(my_string)
-    a = l[num1]
-    b = l[num2]
-    l[num1] = b
-    l[num2] = a
-
-    s = ''
-    for i in l:
-        s += i
-    return s
+    l[num1], l[num2] = l[num2], l[num1]
+    return ''.join(l)
