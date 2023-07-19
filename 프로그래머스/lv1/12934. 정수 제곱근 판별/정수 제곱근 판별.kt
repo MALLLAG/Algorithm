0681b0ad79fb6ed219d45080d068c794cb83d993
김성현 <beyool95@naver.com>
2023-07-19 20:45:43 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -0,0 +1,11 @@
+import kotlin.math.*
+
+class Solution {
+    fun solution(n: Long): Long {
+        return if (sqrt(n.toDouble()) % 1 == 0.0) {
+            (sqrt(n.toDouble()) + 1).pow(2).toLong()
+        } else {
+            -1
+        }
+    }
+}
