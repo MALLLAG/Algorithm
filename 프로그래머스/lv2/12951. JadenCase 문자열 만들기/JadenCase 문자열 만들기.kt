9c3ecadfa4745f181df40148cb8468e85f4ffac6
김성현 <beyool95@naver.com>
2023-08-30 11:30:50 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 14.00 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(s: String): String {
+        return s.split(" ")
+            .joinToString(" ") {
+                it.lowercase().replaceFirstChar { it.uppercase() }
+            }
+    }
+}
