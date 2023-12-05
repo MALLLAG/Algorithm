a96bfb74a9be29a6527accfb2142ff7092f456bf
김성현 <beyool95@naver.com>
2023-12-05 14:56:39 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 16.03 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(
+        strings: Array<String>,
+        n: Int
+    ): Array<String> {
+        return strings.sortedWith(compareBy({ it[n] }, { it }))
+            .toTypedArray()
+    }
+}
