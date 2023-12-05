dff1e8dbb0ab7b2af49349125b7c40c3ac8b5643
김성현 <beyool95@naver.com>
2023-12-05 12:07:09 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(
+        number: IntArray
+    ): Int {
+        var result = 0
+        
+        for (i in 0 until number.size) {
+            for (j in i + 1 until number.size) {
+                for (k in j + 1 until number.size) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        result++
+                    }
+                }
+            }
+        }
+        
+        return result
+    }
+}
